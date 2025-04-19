dc63c297fab4c777a632db5b3454d36bfde96fe7
helenanita <30820814+helenanita@users.noreply.github.com>
2025-04-19 20:07:32 +0530

    latest push

--- gitngroktest.java
+++ gitngroktest.java
@@ -1,11 +1,13 @@
 public class hello{
-  public static void main(String args[])
+  public static void main(String []args)
   {
     System.out.println("Hello helen");
     System.out.println("How are you doing");
     System.out.println("Hey Joanna");
     System.out.println("Hey Jenson");
      System.out.println("Hey ____");
+    System.out.println("i am");
+    
     
   }
   
